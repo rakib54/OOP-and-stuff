8766941b86608479fdc787fc1dc1b1900c319a52
rakib54 <rakibur5454@gmail.com>
2022-10-27 00:05:57 +0600

    send parameter in different ways in inheritance

--- people/human.py
+++ people/human.py
@@ -5,15 +5,29 @@ class Human:
     self.height = height
     self.weight = weight
 
-  def get_name(self):
-    print(f"Name is {self.name}")
+class CseEngineer(Human):
+  def __init__(self,name,gender,height,weight,position,love_to_code):
+    self.position = position
+    self.love_to_code = love_to_code
+    super().__init__(name,gender,height,weight)
 
-class Engineer(Human):
-  def __init__(self,name):
-    self.name = name
+class Police(Human):
+  def __init__(self,name,gender,height,weight,nationality,nid):
+    self.nationality = nationality
+    self.nid = nid
+    super().__init__(name,gender,height,weight)
 
-  # def get_name(self):
-  #   print(f"Name was {self.name}")
+  def Nationality(self):
+    print(f"Nationality is {self.nationality}")
 
-me = Engineer('Rakib')
-me.get_name()
+
+me = CseEngineer('Tamim','Male',65,70,"Software Engineer",True)
+print(me.love_to_code)
+print(me.position)
+
+# we can set value by parameter name. so we don't have to maintain order
+eng = CseEngineer(position="Software Engineer", love_to_code = True, name="Rakib",gender='male',height=70,weight=52,)
+print(eng.name)
+
+police = Police('Rafiq','Male',65,70,'Bangladeshi',True)
+police.Nationality()
