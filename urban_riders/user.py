5e21e854afd760460385bdf536030a761a33b882
rakib54 <rakibur5454@gmail.com>
2022-10-31 00:45:21 +0600

    create user for urban riders

--- urban_riders/user.py
+++ urban_riders/user.py
@@ -32,5 +32,38 @@ class User:
       print("invalid user")
 
 
+class Rider(User):
+  def __init__(self,name, email, password,location,balance):
+    self.location = location
+    self.balance = balance
+    super().__init__(name, email, password)
+
+  def set_location(self,location):
+    self.location = location
+
+  def get_location(self,location):
+    return self.location
+
+  def request_trip(self,destination):
+    pass
+
+  def start_trip(self,fair, destination):
+    self.balance -= fair
+
+
+class Driver(User):
+  def __init__(self,name, email, password,location,license):
+    super().__init__(name, email, password)
+    self.location = location
+    self.license = license
+    self.earning = 0
+
+  def start_trip(self,destination,fair):
+    self.earning += fair
+    self.location = destination
+
+
+
+
 me = User('rakib', "rakib54@gmail.com", 'hero')
 User.log_in("rakib54@gmail.com", 'hero')
