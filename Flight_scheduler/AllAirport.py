082cfee2abc00fee04e06385c4d1a97b7ce197e4
rakib54 <rakibur5454@gmail.com>
2022-11-06 17:12:47 +0600

    find distance between two airports

--- Flight_scheduler/AllAirport.py
+++ Flight_scheduler/AllAirport.py
@@ -44,21 +44,35 @@ class AllAirport:
         
     self.airports = airports
     
-    for airport in airports.items():
-      print(airport)
+    # for airport in airports.items():
+    #   print(airport)
       
-  def distance_between_two_airports(self,lat1,lon1,lat2,lon2):
+  def get_distance_between_two_airports(self,lat1,lon1,lat2,lon2):
     radius = 6371
     diff_lat = radians(lat1 - lat2)
     diff_lon = (lon1 - lon2)
     
     a = (sin(diff_lat / 2) * sin(diff_lon / 2) + cos(radians(lat1)) * cos(radians(lat2)) *
-         math.sin(diff_lon / 2) * math.sin(diff_lon / 2))
+         sin(diff_lon / 2) * sin(diff_lon / 2))
     
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    
     distance = radius * c
     return distance
+  
+  def distance_between_two_airports(self,airport1_code1,airport2_code):
+    airport1 = self.airports[airport1_code1]
+    airport2 = self.airports[airport2_code]
+    distance = self.get_distance_between_two_airports(airport1.lat,airport1.lon,airport2.lat,airport2.lon)
+    return distance
+  
+  def get_ticket_price(self,start,end):
+    distance = self.distance_between_two_airports(start,end)
+    airport1 =self.airports[start]
+    fare = distance * airport1.rate
+    return fare
     
     
-AllAirport()
+    
+world_tour = AllAirport()
+argentina_tour = world_tour.get_ticket_price('DAC', 'PRA')
+print('ticket fare to argentina: ',argentina_tour)
