80ac7eaae2b400395178678e3f3d140e66e6a34a
rakib54 <rakibur5454@gmail.com>
2022-11-17 00:36:43 +0600

    create and read data with api response

--- Api/app.py
+++ Api/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+data = {"rakib": '54'}
 
 @app.route('/', methods=['GET'])
 def home():
@@ -9,6 +11,17 @@ def home():
 @app.route('/about', methods=['GET'])
 def about():
   return "This is about page"
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+  return data
+
+# add_data/?key=value
+@app.route('/add_data', methods=['GET', 'PUT'])
+def add_data():
+  key, value = list(request.args.items())[0]
+  data[key] = value
+  return f"{key} added"
 
 if __name__ == '__main__':
   app.run()
