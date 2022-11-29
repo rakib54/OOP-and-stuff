cb1e6be7948f65ab46861a0df89a59f3fcb7ea1a
rakib54 <rakibur5454@gmail.com>
2022-11-29 23:37:41 +0600

    exploring multiple inheritance

--- inheritance.py
+++ inheritance.py
@@ -11,5 +11,26 @@ class Bus(Vehicle):
     def info(self):
         print(self.speed)
 
-my_bus = Bus('8',10000,120)
-my_bus.info()
+# my_bus = Bus('8',10000,120)
+# my_bus.info()
+
+# multiple Inheritance
+
+class Grandparent:
+  def __init__(self):
+    self.string1 = "I am from a grandparent"
+        
+class Parent(Grandparent):
+    def __init__(self):
+        self.string1 = "I am from a Parent"
+        # super().__init__()
+        
+    
+class Child(Parent,Grandparent):
+    def __init__(self):
+        self.string1 = "I am from a Child"
+        Parent.__init__(self)
+        Grandparent.__init__(self)
+    
+me = Child()
+print(me.string1)
